32885fef0801cbf4d93d3c24ec64be14e8ae95ca
DEADPOOL712 <saneparavaibhav73@gmail.com>
2023-07-21 13:45:54 +0530

    generics

--- FileInputOutput/src/App.java
+++ FileInputOutput/src/App.java
@@ -1,8 +1,59 @@
 import java.io.*;
+import java.util.*;
 
-// File Input Output 
+//! generic class
+class Printer<T> {
+    T thingsToPrint;
+    Shout s = new Shout();
+
+    Printer(T thingsToPrint) {
+        this.thingsToPrint = thingsToPrint;
+    }
+
+    void display() {
+        System.out.println(thingsToPrint);
+        s.shout(thingsToPrint, "HEYYY"); // anything will be fine here int,double,string
+
+    }
+
+}
+
+// ! generic method
+class Shout {
+    <T, F> void shout(T messageToShout, F otherMessage) {
+        System.out.println(messageToShout + "!!!!!");
+        System.out.println(otherMessage + "!!!!");
+    }
+}
+
+// ! concept of wild card
+class WildCard {
+    // when we dont know what kind of collection it is
+    void printList(List<?> il) { // "?" is wild card
+        System.out.println(il);
+    }
+    // below method is not valid
+    // void printList(List<Object> il)
+}
+
+// File Input Output
 public class App {
     public static void main(String[] args) throws Exception {
+
+        Printer<Integer> ip = new Printer<>(5);
+        Printer<String> sp = new Printer<>("Hellow");
+        Printer<Double> dp = new Printer<>(123.33);
+        dp.display();
+        sp.display();
+        ip.display();
+
+        // wild card demo
+        List<Integer> il = new ArrayList<>();
+        WildCard wc = new WildCard();
+        il.add(123);
+        il.add(33);
+        wc.printList(il);
+
         // ! File calss -> for creating , deleting , searching file
         // File fl = new File("index.txt");
         // System.out.println(fl.getName());
@@ -31,26 +82,27 @@ public class App {
         // ! Character Stream -> read/write in character
         // ! Reader / Writer are base class of all below methods
         // ================================================//
-        Reader reader = new FileReader("xyz.txt"); // will not create file
+        // Reader reader = new FileReader("xyz.txt"); // will not create file
         // reader.read() -> will read only one byte
 
-        Writer writer = new FileWriter("xyz.txt");
+        // Writer writer = new FileWriter("xyz.txt");
         // writer.write("Hellow new file!"); -> can write sting directly
 
         // ================================================//
-        BufferedReader bfr = new BufferedReader(reader);
-        System.out.println(bfr.readLine()); // read complete line
+        // BufferedReader bfr = new BufferedReader(reader);
+        // System.out.println(bfr.readLine()); // read complete line
 
-        BufferedWriter bwr = new BufferedWriter(writer);
-        bwr.write("This is new text of new file!!!");
+        // BufferedWriter bwr = new BufferedWriter(writer);
+        // bwr.write("This is new text of new file!!!");
 
         // ================================================//
         // StringReader srd = new StringReader(reader);
         // only deals with strings not file directly
 
-        bfr.close();
-        bwr.close();
-        writer.close();
-        reader.close();
+        // bfr.close();
+        // bwr.close();
+        // writer.close();
+        // reader.close();
+
     }
 }
